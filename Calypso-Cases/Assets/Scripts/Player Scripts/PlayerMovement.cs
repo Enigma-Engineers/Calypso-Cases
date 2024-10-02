5c971fc5f7129e466a8df2784b378c72fe8c1826
Treynenni <treynenni@gmail.com>
2024-10-01 20:41:51 -0400

    Movement utilizes Unity input system

--- Calypso-Cases/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Calypso-Cases/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -2,40 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
 
     Vector2 movement;
 
-    bool actionInput;
-
-    // Update is called once per frame
-    void Update()
-    {
-        PlayerInput();
-    }
-
     void FixedUpdate()
     {
         //Updates the characters movement based on what key is pressed and the movement speed provided
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
-        //TODO: Get a reference to an object to interact with
-        //      Allow an interaction between the player and the object (maybe like a text box that says you interacted with it, maybe you pick it up)
-        if (actionInput)
-        {
-            //Interacts with item
-        }
     }
 
-    void PlayerInput()
+    public void OnMove(InputAction.CallbackContext ctx)
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        actionInput = Input.GetKeyDown(KeyCode.E);
+        movement = ctx.ReadValue<Vector2>();
     }
 }
