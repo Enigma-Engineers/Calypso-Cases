41c98ebb1d37a5cb063498ea3700070e1e92dd6b
julianalvia422 <jaa2128@g.rit.edu>
2024-10-22 15:56:34 -0400

    Added basic choice selection to NPCs

--- Calypso-Cases/Assets/Dialogue/test.ink
+++ Calypso-Cases/Assets/Dialogue/test.ink
@@ -1,2 +1,20 @@
-Here's some text
-Here's some more text
+-> main
+
+=== main ===
+Do you like cats or dogs?
++ [Cats]
+    -> cats
++ [Dogs]
+    -> dogs
+
+
+=== cats ===
+To be honest I don't like cats
+-> END
+
+=== dogs ===
+I love dogs!!!
+-> END
+
+
+
--- Calypso-Cases/Assets/Scripts/Dialogue Scripts/DialogueManager.cs
+++ Calypso-Cases/Assets/Scripts/Dialogue Scripts/DialogueManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using TMPro;
 using Ink.Runtime;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using System.Net.Security;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -49,9 +51,9 @@ public class DialogueManager : MonoBehaviour
 
         // get all of the choices text
         choicesText = new TextMeshProUGUI[choices.Length];
-        for(int i = 0; i < choices.Length; i++)
+        for (int i = 0; i < choices.Length; i++)
         {
-            choicesText[i] = choices[i].GetComponent<TextMeshProUGUI>();
+            choicesText[i] = choices[i].GetComponentInChildren<TextMeshProUGUI>();
         }
     }
 
@@ -76,6 +78,9 @@ public class DialogueManager : MonoBehaviour
             // displays next line of dialogue in ink file
             // similar to a Queue
             dialogueText.text = currentStory.Continue();
+
+            // display choices, if any, for this dialogue line
+            DisplayChoices();
         }
         else
         {
@@ -95,10 +100,41 @@ public class DialogueManager : MonoBehaviour
     {
         List<Choice> currentChoices = currentStory.currentChoices;
 
+        // Defensive check to make sure our UI can support the number of choices coming in
         if (currentChoices.Count > choices.Length)
         {
             Debug.LogError("More choices were given than the UI can support. " +
                 "Number of choices given: " + currentChoices.Count);
         }
+
+        // enable and initialize the choices up to the amount of choices for this Line of dialogue
+        for (int i = 0; i < currentChoices.Count; i++)
+        {
+            choices[i].gameObject.SetActive(true);
+            choicesText[i].text = currentChoices[i].text;
+        }
+
+        // go through the remaining choices the UI supports and make sure they are hidden
+        for (int i = currentChoices.Count; i < choices.Length; i++)
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+
+        StartCoroutine(SelectFirstChoice());
+    }
+
+    private IEnumerator SelectFirstChoice()
+    {
+        // Event System requires we clear the first selected choice first,
+        // then wait for atleast one frame before we set the selected choice
+        EventSystem.current.SetSelectedGameObject(null); yield return null;
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    } 
+
+    public void MakeChoice(int choiceIndex)
+    {
+        currentStory.ChooseChoiceIndex(choiceIndex);
+        ContinueStory();
     }
 }
