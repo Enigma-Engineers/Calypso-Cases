60c1f68848d67256583440ffc935cd0bd2b71fcf
Jye Crocker <jyefett22@gmail.com>
2024-11-13 04:02:56 -0500

    2nd scene has transition added, still need to add gameplay content but the structure is there.

--- Calypso-Cases/Assets/Scripts/Menu/VictoryScreen.cs
+++ Calypso-Cases/Assets/Scripts/Menu/VictoryScreen.cs
@@ -1,24 +1,46 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class VictoryScreen : MonoBehaviour
 {
     private bool hasWon = false;
     [SerializeField] private CaseData caseData;
     [SerializeField] private GameObject winCanvas;
+    [SerializeField] private Image fadeOverlay;  // UI Image for the fade effect
+    [SerializeField] private float fadeDuration = 1.0f;  // Duration of the fade effect
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (hasWon == false)
+        if (!hasWon)
         {
             hasWon = caseData.Compare();
         }
         else
         {
             winCanvas.SetActive(true);
+            StartCoroutine(FadeAndLoadScene("Level_2"));
         }
+    }
+
+    private IEnumerator FadeAndLoadScene(string sceneName)
+    {
+        float elapsedTime = 0f;
+        Color color = fadeOverlay.color;
+        color.a = 0f;  // Start with a fully transparent image
+        fadeOverlay.color = color;
+
+        // Fade to opaque
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Clamp01(elapsedTime / fadeDuration);
+            fadeOverlay.color = color;
+            yield return null;
+        }
+
+        // Load the next scene
+        SceneManager.LoadScene(sceneName);
     }
 }
