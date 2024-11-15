7568d493a5b32ef7a2e417b7c7dd8e4cbf15d87d
Treynenni <treynenni@gmail.com>
2024-11-14 21:21:03 -0500

    Build submitted, title screen fixed

--- Calypso-Cases/Assets/Scripts/Corkboard/HoverHighlight.cs
+++ Calypso-Cases/Assets/Scripts/Corkboard/HoverHighlight.cs
@@ -2,23 +2,25 @@ using UnityEngine;
 
 public class HoverHighlight : MonoBehaviour
 {
-    private Renderer objectRenderer;
+    private SpriteRenderer objectRenderer;
     private Color originalColor;
     private Color highlightColor = new Color(0f, 0f, 1f, 0.95f); // Light transparent blue
 
     private void Awake()
     {
-        objectRenderer = GetComponent<Renderer>();
+        objectRenderer = GetComponent<SpriteRenderer>();
         originalColor = objectRenderer.material.color;
     }
 
     private void OnMouseEnter()
     {
         objectRenderer.material.color = highlightColor;
+            
     }
 
     private void OnMouseExit()
     {
         objectRenderer.material.color = originalColor;
+
     }
 }
